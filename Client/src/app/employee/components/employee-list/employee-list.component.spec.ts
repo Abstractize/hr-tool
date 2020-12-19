b64d2a3144246cbbc9a0de157c96e69ff24e26b1
Gabriel Abarca <gababarca@hotmail.com>
2020-12-18 19:50:50 -0600

    [Add] Tests

--- Client/src/app/employee/components/employee-list/employee-list.component.spec.ts
+++ Client/src/app/employee/components/employee-list/employee-list.component.spec.ts
@@ -1,14 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { Employee } from '../../models/employee';
+import { EmployeeService } from '../../services/employee/employee.service';
+import { Image } from '../../models/image';
 import { EmployeeListComponent } from './employee-list.component';
 
 describe('EmployeeListComponent', () => {
   let component: EmployeeListComponent;
   let fixture: ComponentFixture<EmployeeListComponent>;
+  let httpMock: HttpTestingController;
+  let service: EmployeeService;
+
+  const emptyEmployee: Employee = new Employee(
+    'id',
+    '',
+    new Image('', 0),
+    '',
+    '',
+    new Date()
+  );
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EmployeeListComponent ]
+      imports: [
+        CommonModule,
+        HttpClientTestingModule,
+        FormsModule
+      ],
+      declarations: [ EmployeeListComponent ],
+      providers: [EmployeeService],
     })
     .compileComponents();
   });
@@ -17,9 +39,23 @@ describe('EmployeeListComponent', () => {
     fixture = TestBed.createComponent(EmployeeListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    httpMock = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(EmployeeService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('shouldFilter', () => {
+    const request = httpMock.expectOne(`${service.url}`);
+    expect(request.request.method).toBe('GET');
+    const resp = emptyEmployee;
+    resp.id = 0;
+    request.flush([resp]);
+    component.filter = 'id';
+    component.search();
+    expect(component.employees).toBeTruthy();
+  });
+
 });
