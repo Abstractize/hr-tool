49d37bcc8d47a67132914a276853dd4ce6182e40
Gabriel Abarca <gababarca@hotmail.com>
2020-12-19 14:00:25 -0600

    [Add] Information Dialogue

---
+++ Client/src/app/core/components/dialog-information/dialog-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-information',
+  templateUrl: './dialog-information.component.html',
+  styleUrls: ['./dialog-information.component.scss']
+})
+export class DialogInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
