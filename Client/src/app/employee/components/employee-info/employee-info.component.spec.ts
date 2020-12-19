4f1720204d121b0704c0b8778d28f912c785df5c
Gabriel Abarca <gababarca@hotmail.com>
2020-12-19 12:59:51 -0600

    [Fix] Tests

--- Client/src/app/employee/components/employee-info/employee-info.component.spec.ts
+++ Client/src/app/employee/components/employee-info/employee-info.component.spec.ts
@@ -1,14 +1,37 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Employee } from '../../models/employee';
+import { Image } from '../../models/image';
+import { EmployeeService } from '../../services/employee/employee.service';
+import { ImageService } from '../../services/image/image.service';
 
 import { EmployeeInfoComponent } from './employee-info.component';
 
 describe('EmployeeInfoComponent', () => {
   let component: EmployeeInfoComponent;
   let fixture: ComponentFixture<EmployeeInfoComponent>;
+  let httpMock: HttpTestingController;
+  let eService: EmployeeService;
+  let iService: ImageService;
+
+  const emptyEmployee: Employee = new Employee(
+    'id',
+    '',
+    new Image('', 0),
+    '',
+    '',
+    new Date()
+  );
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EmployeeInfoComponent ]
+      imports:[
+        HttpClientTestingModule,
+      ],
+      declarations: [ EmployeeInfoComponent ],
+      providers: [NgbActiveModal, ImageService]
     })
     .compileComponents();
   });
@@ -16,7 +39,9 @@ describe('EmployeeInfoComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(EmployeeInfoComponent);
     component = fixture.componentInstance;
+    component.employee = emptyEmployee;
     fixture.detectChanges();
+
   });
 
   it('should create', () => {
